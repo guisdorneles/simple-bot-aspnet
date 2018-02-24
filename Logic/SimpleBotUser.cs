f94681d0ce52c865ca6b9f5f2c13c29d93dc3f96
guisdorneles <guisd58@hotmail.com>
2018-02-24 13:35:29 -0300

    tarefa 3 - perfil usuario

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -9,29 +9,50 @@ namespace SimpleBot
 {
     public class SimpleBotUser
     {
-        static Dictionary<string, UserProfile> _perfil = new Dictionary<string, UserProfile>();
+        ///static Dictionary<string, UserProfile> _perfil = new Dictionary<string, UserProfile>();
         public static string Reply(Message message)
+        {
+            string userId = message.Id;
+            var perfil = GetProfile(userId);
+            perfil.Visitas += 1;
+            SetProfile(userId, perfil);
+            //return $"{message.User} disse '{message.Text}'";
+            return $"{message.User} conversou {perfil.Visitas} vezes";
+        }
+
+        public static void SalvaHistorico(Message message)
         {
             var cliente = new MongoClient();
             var db = cliente.GetDatabase("bot");
             var col = db.GetCollection<BsonDocument>("historico");
+
+            int qtdeMensagem = PegaQtdeMensagem();
             var doc = new BsonDocument()
             {
                 {"Nome Usuario", message.User },
                 {"Texto", message.Text},
             };
             col.InsertOne(doc);
-            string userId = message.Id;
-            var perfil = GetProfile(userId);
-            perfil.Visitas += 1;
-            SetProfile(userId,)
-            return $"{message.User} disse '{message.Text}'";
+        }
+
+        public static int PegaQtdeMensagem()
+        {
+            int qtdeMensagem = 0;
+
+            return qtdeMensagem;
         }
 
         public static UserProfile GetProfile(string id)
         {
-            if(_perfil.ContainsKey(id))
-                return _perfil[id];
+            //Buscar o usuario 
+            var usuario = new MongoClient();
+            var db = usuario.GetDatabase("bot");
+            var col = db.GetCollection<UserProfile>("usuario");
+            // Procurar no banco se não existe cria em memoria
+            var listaUsuario = col.Find(new BsonDocument("_id", id)).FirstOrDefault();
+            if (listaUsuario != null)
+                return listaUsuario;
+
             return new UserProfile
             {
                 Id = id,
@@ -41,7 +62,11 @@ namespace SimpleBot
 
         public static void SetProfile(string id, UserProfile profile)
         {
-            _perfil[id] = profile;
+            var usuario = new MongoClient();
+            var db = usuario.GetDatabase("bot");
+            var col = db.GetCollection<UserProfile>("usuario");
+            // Procurar no banco se não existe cria, se sim atualiza.
+            col.ReplaceOne(new BsonDocument("_id", id), profile, new UpdateOptions() { IsUpsert = true });
         }
     }
 }
