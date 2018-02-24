1554cbf0524382409560bbf8a748d99369641e50
guisdorneles <guisd58@hotmail.com>
2018-02-24 11:14:37 -0300

    Commit

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,18 +9,39 @@ namespace SimpleBot
 {
     public class SimpleBotUser
     {
+        static Dictionary<string, UserProfile> _perfil = new Dictionary<string, UserProfile>();
         public static string Reply(Message message)
         {
+            var cliente = new MongoClient();
+            var db = cliente.GetDatabase("bot");
+            var col = db.GetCollection<BsonDocument>("historico");
+            var doc = new BsonDocument()
+            {
+                {"Nome Usuario", message.User },
+                {"Texto", message.Text},
+            };
+            col.InsertOne(doc);
+            string userId = message.Id;
+            var perfil = GetProfile(userId);
+            perfil.Visitas += 1;
+            SetProfile(userId,)
             return $"{message.User} disse '{message.Text}'";
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            if(_perfil.ContainsKey(id))
+                return _perfil[id];
+            return new UserProfile
+            {
+                Id = id,
+                Visitas = 0
+            };
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            _perfil[id] = profile;
         }
     }
 }
